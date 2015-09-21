ab0b79b4084e7e54a0a19e6f8e3ebf0782794c6a
gr4y <swessel@gr4yweb.de>
2015-09-21 13:46:27 +0200

    moved example-itunes_feed.xml to subdirectory

--- poo_test.go
+++ poo_test.go
@@ -25,7 +25,7 @@ func TestWithString(t *testing.T) {
 	if testing.Short() {
 		t.Skip("Skipping test in short mode.")
 	}
-	xmlFeed := readXML("example-itunes_feed.xml")
+	xmlFeed := readXML("feeds/example-itunes_feed.xml")
 	feed, err := poo.ParseXML(xmlFeed)
 	if err != nil {
 		t.Error(err)
